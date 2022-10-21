8e39e1b9d24a24ee591e72262027fb5bae6ca15d
Joseph Wambura <joseph.wambura@pycs.co.ke>
2022-10-21 09:45:26 +0300

    Add project files.

---
+++ PrintColour/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("--> Print the color black (on a white background)");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("--> Print the color dark blue");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("--> Print the color dark green");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("--> Print the color dark cyan");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("--> Print the color dark red");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("--> Print the color dark magenta");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("--> Print the color dark yellow");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("--> Print the color gray");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("--> Print the color dark gray");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("--> Print the color blue");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("--> Print the color green");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("--> Print the color cyan");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("--> Print the color red");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("--> Print the color magenta");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("--> Print the color yellow");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("--> Print the color white");
+Console.ResetColor();
